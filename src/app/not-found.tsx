b67285b59ca8e79c0f53ee6a493617ddc7696295
whoiseon <whois__@naver.com>
2024-01-10 22:40:20 +0900

    feat: routes grouping

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function NotFound() {
+  return (
+    <div>
+      <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해보세요.</div>
+      <Link href="/search">검색</Link>
+    </div>
+  )
+}
+
+export default NotFound;
